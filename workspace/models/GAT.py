1d23e9b868d0ef271b955652740b1a7f292a5547
Gustaw Andrzejewski <gustaw.andrzejewski@companyhouse.de>
2023-07-06 22:40:27 +0200

    initial models for testing

---
+++ workspace/models/GAT.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.data import Data
+from torch_geometric.nn import GATConv
+
+
+class GAT(torch.nn.Module):
+    def __init__(
+        self,
+        num_node_features: int,
+        num_classes: int,
+        num_hidden_units: int,
+        dropout: float = 0.5,
+    ):
+        super(GAT, self).__init__()
+
+        self.conv1 = GATConv(num_node_features, num_hidden_units)
+        self.conv2 = GATConv(num_hidden_units, num_classes)
+        self.dropout = torch.nn.Dropout(dropout)
+
+    def forward(self, data: Data) -> torch.Tensor:
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.conv2(x, edge_index)
+
+        return torch.sigmoid(x)
